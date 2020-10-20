df9584bc306a79436afd9b72aba22df0efd51bba
Admin <1139287223@qq.com>
2020-10-20 19:31:37 +0800

    default

---
+++ test.py
@@ -0,0 +1,8 @@
+url = [{"商务发展中心":"http://192.168.101.203/prcnt.htm"},{"分析测试中心-2F":"http://192.168.102.253/prcnt.htm"},{"分析测试中心-4F":"http://192.168.104.251/prcnt.htm"},{"公共使用-6F":"http://192.168.106.251/prcnt.htm"},]
+ua = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36 Edg/86.0.622.38"}
+
+
+# a = {"a":"b","c":"d"}
+# print(a.items())
+import time
+print(time.strftime("%Y-%m-%d",time.localtime()))
---
+++ 打印机统计.py
@@ -0,0 +1,10 @@
+import  requests
+import re
+import xlwt
+import time
+
+url = ["http://192.168.101.253/prcnt.htm","http://192.168.104.253/prcnt.htm"]
+l = len(url)
+for i in range(0,l) :
+    print(i)
+
