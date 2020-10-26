fdb7496fef1c57241b9ca8ccbdcdadfd53ed64f5
笔记本 <1139287223@qq.com>
2020-10-26 14:45:39 +0800

    笔记本

---
+++ xpath作业-爬取站长素材免费简历.py
@@ -0,0 +1,16 @@
+import requests
+from lxml import etree
+url = "http://sc.chinaz.com/jianli/free.html"
+ua = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.80 Safari/537.36 Edg/86.0.622.48'}
+
+respone = requests.get(url=url,headers=ua)
+jianli_page = bytes(respone.text,respone.encoding).decode('utf-8','ignore')
+
+tree = etree.HTML(jianli_page)
+jianli_list = tree.xpath('//div[@id="main"]/div/div')
+for jianli in jianli_list:
+    jl_url = jianli.xpath('./a/@href')[0]
+    jl_name = jianli.xpath('./a/img/@alt')[0]
+    respone2 = requests.get(url=jl_url,headers=ua)
+    jianli2_page = bytes(respone2.text,respone2.encoding).decode('urt-8','ignore')
+
