e2abcac600ea80a1cce4aa89e28e1aaddb7814f5
笔记本 <1139287223@qq.com>
2020-10-27 10:11:33 +0800

    笔记本

--- test.py
+++ test.py
@@ -1,12 +1,13 @@
 import requests
 from lxml import etree
-url ='http://downsc.chinaz.net/Files/DownLoad/jianli/202010/jianli13852.rar'
+#url ='http://downsc.chinaz.net/Files/DownLoad/jianli/202010/jianli13852.rar'
 ua = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36 Edg/86.0.622.38"}
 
-
-txt = requests.get(url=url,headers=ua).content
-with open('./jianli.rar','wb') as fp :
-    fp.write(txt)
-
+url = "http://pic.netbian.com/4kmeinv/"
+url_page = requests.get(url=url, headers=ua).text
+r_tree = etree.HTML(url_page)
+page = r_tree.xpath('//div[@class="page"]/a')
+r = page[-2]
+print(str(r.xpath('./text()')[0]))
 
 
