2b01b8d6b1d56bf1c1c4630020718d74ea4ac16e
zhangsir <1139287223@qq.com>
2020-11-06 13:34:40 +0800

    default

---
+++ 爬虫课件/第七章：动态加载数据处理/test.py
@@ -0,0 +1,18 @@
+# from selenium import webdriver
+# from time import sleep
+# from selenium.webdriver.chrome.options import Options
+#
+# chrome_options = Options()
+# chrome_options.add_argument('headless')
+# chrome_options.add_argument('--disable-gpu')
+#
+# option = webdriver.ChromeOptions()
+# option.add_experimental_option('excludeSwitches',['enable-automation'])
+#
+# bro = webdriver.Chrome(executable_path='./chromedriver',chrome_options=chrome_options,options=option)
+#
+# bro.get('https://www.baidu.com')
+# sleep(2)
+# bro.quit()
+from selenium import webdriver
+bro = webdriver.Chrome(executable_path='./chromedriver')
