61c4f6676a449928d5b454a9092a9dfacc72d577
Admin <1139287223@qq.com>
2020-11-06 13:32:31 +0800

    default

--- selenium/无头+反检.py
+++ selenium/无头+反检.py
@@ -15,7 +15,7 @@ option = ChromeOptions()
 option.add_experimental_option('excludeSwitches', ['enable-automation'])
 
 #如何实现让selenium规避被检测到的风险
-bro = webdriver.Edge(executable_path='./msedgedriver.exe',)
+bro = webdriver.Chrome(executable_path='./chromedriver.exe',options=option,chrome_options=chrome_options)
 
 #无可视化界面（无头浏览器） phantomJs
 bro.get('https://www.baidu.com')
